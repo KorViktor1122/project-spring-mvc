a560663b4d4bd4d59643a1ab8dd7e3f2713d5fb7
KorViktor1122 <59253167+KorViktor1122@users.noreply.github.com>
2020-09-19 15:49:30 +0300

    Second commit

--- src/main/java/v/korneev/spring/mvc/ProductControler.java
+++ src/main/java/v/korneev/spring/mvc/ProductControler.java
@@ -1,9 +1,7 @@
 package v.korneev.spring.mvc;
 
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,15 +15,6 @@ public class ProductControler<CartService> {
         this.cartService = cartService;
     }
 
-//    @GetMapping("/products")
-//    public String getAllproducts (Product product){
-//        product.
-//    }
-//
-//    @PostMapping("/add")
-//    public String addProducts(Product product){
-//        cartService.
-//        return "redirect:/products/all";
-//    }
+
 
 }
